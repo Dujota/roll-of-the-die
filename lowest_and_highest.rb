396041a3040e5202a86946c5b8f1dd872d4d13e3
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-09-24 21:05:11 -0400

    completed the soreted rolls, had to fix the previous ex as the way i iterated through the array worked there but not on this example

--- lowest_and_highest.rb
+++ lowest_and_highest.rb
@@ -1,7 +1,7 @@
 
 sorted_rolls = []
 
-10.times do
+5.times do
   dice = Random.rand(1..6)
   sorted_rolls << dice
 end
@@ -9,3 +9,8 @@ end
 sorted_rolls.sort!
 
 sorted_rolls.each { |rolls| puts "The result of your roll is #{rolls}" }
+
+
+puts "-"*33
+puts "the lowest number is #{sorted_rolls.first}"
+puts "the lowest number is #{sorted_rolls.last}"
