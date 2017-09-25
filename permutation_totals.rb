14a4b24229656f0c13f51d05f6d11769c168b97b
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-09-24 21:54:24 -0400

    added another solution to the repeat permutation using an array as the input

---
+++ permutation_totals.rb
@@ -0,0 +1,5 @@
+(1..6).each { |die1|
+  (1..6).each { |die2|
+    puts "#{die1} #{die2}"
+  }
+}
--- permutations.rb
+++ permutations.rb
@@ -1,5 +1,17 @@
+# Write a program that goes through all possible permutations of two dice rolling.
+
+# tried with just iteration of values 1-6 for two dice and printing both out
+
 (1..6).each { |die1|
   (1..6).each { |die2|
     puts "#{die1} #{die2}"
   }
 }
+
+
+# from searching on google got an array solution and was able to put this together with
+# the repeated_permutation examples in ruby-docs + my searching which let me understand to use join
+
+dice_rolls = *(1..6)
+
+dice_rolls.repeated_permutation(2) { |rolls| puts rolls.join(' ') }
